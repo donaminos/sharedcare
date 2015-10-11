bea58ad92589ff7b17d6e8e12971f9f3045f7240
David Baxter <david@bizarmobile.com.au>
2015-10-11 13:08:59 +1100

    styling

--- client/views/newsfeed/addNewsItem.js
+++ client/views/newsfeed/addNewsItem.js
@@ -2,16 +2,17 @@ Template.addNewsItem.events({
     'click .save': function(e) {
         e.preventDefault();
         var postText = $("#postText").val();
-        if ( !! postText) {
+        if (!!postText || !!Session.get('photo')) {
             var user = Meteor.user();
             News.insert({
                 postText: postText,
                 group: user.profile.groupId[0],
                 userId: Meteor.userId(),
                 createdAt: new Date,
-                attachement: Session.get('photo') ? Session.get('photo') : "",
+                attachment: Session.get('photo') ? Session.get('photo') : "",
                 comments: []
             });
+            Session.set('photo', null);
             IonModal.close('addNewsItem');
         } else {
             IonModal.close('addNewsItem');
