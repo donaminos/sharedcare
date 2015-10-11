a0ddb06bcb06c633aaff7c19e7757962fb445432
Vikas Jayaram <vikas@bizarmobile.com.au>
2015-10-11 11:08:25 +1100

    Camera API

--- client/views/expenses/expenses.js
+++ client/views/expenses/expenses.js
@@ -7,7 +7,8 @@ Template.expenses.events({
 Template.expenses.helpers({
     expenseList: function() {
         var list = [];
-        var expenses = Expenses.find({group: 'abcd'}, { sort: { date: -1 }}).fetch();
+        var user = Meteor.user();
+        var expenses = Expenses.find({group: user.profile.groupId[0]}, { sort: { date: -1 }}).fetch();
         var date = '';
         for (var i = 0; i < expenses.length; i++) {
         console.log(expenses[i].date, typeof expenses[i].date, date, typeof date);
@@ -23,4 +24,15 @@ Template.expenses.helpers({
     }
 });
 Template.singleExpenseItem.helpers({
+    isAttachment: function(attachment) {
+        return attachment == "" ? false : true;
+    }
 });
+
+Template.singleExpenseItem.events({
+    'click .viewAttachment': function(e) {
+        e.preventDefault();
+        console.log('this', this);
+        IonModal.open('showAttachment', this);
+    }
+})
--- client/views/newsfeed/addNewsItem.js
+++ client/views/newsfeed/addNewsItem.js
@@ -2,8 +2,26 @@ Template.addNewsItem.events({
   'click .save': function(e) {
     e.preventDefault();
     var postText = $("#postText").val();
-    var user = '1234'; //Meteor.userId()
-    News.insert({postText: postText, group: 'abcd', user: user, createdAt: new Date, comments: []});
+    var user = Meteor.user();
+    News.insert({
+    	postText: postText,
+    	group: user.profile.groupId[0],
+    	userId: Meteor.userId(),
+    	createdAt: new Date,
+    	attachement: Session.get('photo') ? Session.get('photo') : "",
+    	comments: []
+    });
     IonModal.close('addNewsItem');
-  }
+  },
+  'click .picture': function () {
+      MeteorCameraUI.getPicture(CameraOptions, function (error, data) {
+        Session.set("photo", data);
+      });
+    }
+});
+
+Template.addNewsItem.helpers({
+	photo: function () {
+  		return Session.get("photo");
+	}
 });
