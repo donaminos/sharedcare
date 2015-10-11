6bf067e6f67bf4bb43c326c151093224f5ee6974
Vikas Jayaram <vikas@bizarmobile.com.au>
2015-10-11 13:50:02 +1100

    subscription for kids.

--- server/publications.js
+++ server/publications.js
@@ -2,9 +2,9 @@ Meteor.publish('countries', function () {
 	return Countries.find({}, {name: 1}, {sort: {name: 1}});
 });
 
-Meteor.publish('kids', function(userId) {
-	console.log(userId);
-  return Children.find();//{userId: {$in: parents}}
+Meteor.publish('kids', function(groupId) {
+	console.log(groupId);
+  return Children.find({groupId: groupId});//{userId: {$in: parents}}
 });
 
 Meteor.publish('kid', function(_id) {
